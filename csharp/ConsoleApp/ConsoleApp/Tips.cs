eb88bf3528f47c3d45390e0e1509b8aa1432c014
Chengming Ruan <v-cruan@microsoft.com>
2021-10-20 10:15:49 +0800

    update

---
+++ csharp/ConsoleApp/ConsoleApp/Tips.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Tips
+    {
+        public void UseEnvironment()
+        {
+            // please use Enviroment Variable to store important information
+            var OS_message = Environment.GetEnvironmentVariable("OS");
+            var ConnStr = Environment.GetEnvironmentVariable("ConnStr");
+            var SqlPassword = Environment.GetEnvironmentVariable("SqlPassword");
+            Console.WriteLine(OS_message);
+            Console.WriteLine(ConnStr);
+            Console.WriteLine(SqlPassword);
+        }
+    }
+}
