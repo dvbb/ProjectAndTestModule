0dd588d4ef04d41ec6161518a2cff9a864b6f53d
Chengming Ruan <v-cruan@microsoft.com>
2021-11-05 16:57:41 +0800

    Optimization WordBook.ps1

--- powershell/WordBook.ps1
+++ powershell/WordBook.ps1
@@ -1,13 +1,15 @@
 # Record high frequency English words
 # EXECUTE: "Word" | .\WordBook.ps1
 
-process {
+process 
+{
     $array = Get-Content .\WordBook.txt
     $append = $false
     for ($i = 0; $i -lt $array.Count; $i++) 
     {
         $strlist = $array[$i].Split(":")
-        if ($strlist[0].Equals($_)) {
+        if ($strlist[0] -eq $_) 
+        {
             $append = $true
             $array[$i] = $strlist[0] + ":" + (1 + $strlist[1])
             BREAK
@@ -17,8 +19,8 @@ process {
     {
         $array | Out-File -FilePath WordBook.txt 
     }
-    else{
-        $_+":1" |  Out-File -FilePath WordBook.txt  -Append
+    else
+    {
+        ($_ -replace ' ','')+":1" |  Out-File -FilePath WordBook.txt  -Append
     }
 }
-
