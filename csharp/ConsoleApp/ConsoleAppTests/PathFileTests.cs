7dd4925962dbb0e35b8acaa3e12b9ce591e8591d
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2021-12-15 11:04:46 +0800

    Directory test

--- csharp/ConsoleApp/ConsoleAppTests/PathFileTests.cs
+++ csharp/ConsoleApp/ConsoleAppTests/PathFileTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleAppTests
@@ -27,7 +28,24 @@ namespace ConsoleAppTests
             Console.WriteLine("GetFileNameWithoutExtension(): " + nameWithoutExtension);
             Console.WriteLine("GetDirectoryName(): " + dir);
             Console.WriteLine("GetPathRoot(): " + root);
-            //string binDirectory = Directory.GetCurrentDirectory();
+        }
+
+        [Test]
+        public void DirectoryTest()
+        {
+            Console.WriteLine("Is [d:\\NewDirectoty] exist? " + Directory.Exists(@"d:\NewDirectoty"));
+            Directory.CreateDirectory(@"d:\NewDirectoty");
+            Console.WriteLine("CreateDirectory()");
+            Console.WriteLine("Is [d:\\NewDirectoty] exist? " + Directory.Exists(@"d:\NewDirectoty"));
+            Console.WriteLine();
+
+            string curDirectory = Directory.GetCurrentDirectory();
+            string[] fileList = Directory.GetFiles(curDirectory);
+            string[] directoryList = Directory.GetDirectories(curDirectory);
+            Console.WriteLine("GetCurrentDirectory(): " + curDirectory);
+            Console.WriteLine("GetFiles() " + fileList + " " + fileList.Count());   //cur下的所有文件
+            Console.WriteLine("GetDirectories() " + directoryList + " " + directoryList.Count()); //cur下的所有子目录
+            Console.WriteLine();
         }
     }
 }
