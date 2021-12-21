205b69d02745365e7a6fe3f5bde1cad6f11f77b1
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2021-12-21 17:39:13 +0800

    Create HttpClientTests.cs

---
+++ csharp/ConsoleApp/ConsoleAppTests/HttpClientTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTests
+{
+    public class HttpClientTests
+    {
+        [Test]
+        public async Task HttpClientTest()
+        {
+            HttpClient client = new HttpClient(new HttpClientHandler()
+            {
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+            });
+            var url = new Uri("https://www.baidu.com");
+            var message = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await client.SendAsync(message);
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response);
+        }
+    }
+}
