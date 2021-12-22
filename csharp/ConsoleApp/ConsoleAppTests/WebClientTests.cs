cb699b9fb6aa5d7c21f47d78c6406894d11428d0
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2021-12-22 13:50:46 +0800

    Create WebClientTests.cs

---
+++ csharp/ConsoleApp/ConsoleAppTests/WebClientTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTests
+{
+    public class WebClientTests
+    {
+        [Test]
+        public void GetImagesTest()
+        {
+            string filePath = "d:\\haha.jpg";
+            Console.WriteLine($"{filePath} does exists?  "+File.Exists(filePath));
+
+            Uri uri = new Uri("http://www.baidu.com/img/flexible/logo/pc/result.png");
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(uri, filePath);
+            Console.WriteLine("success");
+            Console.WriteLine($"{filePath} does exists?  "+File.Exists(filePath));
+
+            File.Delete(filePath);
+        }
+    }
+}
