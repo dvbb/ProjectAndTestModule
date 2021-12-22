ef672ef276609abc432d1b454e951234c2281c91
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2021-12-22 13:50:39 +0800

    Update HttpClientTests.cs

--- csharp/ConsoleApp/ConsoleAppTests/HttpClientTests.cs
+++ csharp/ConsoleApp/ConsoleAppTests/HttpClientTests.cs
@@ -1,6 +1,9 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,19 +12,30 @@ namespace ConsoleAppTests
 {
     public class HttpClientTests
     {
+        private Uri _uri = new Uri("https://www.baidu.com");
+
         [Test]
-        public async Task HttpClientTest()
+        public async Task HttpClientBasicTest()
+        {
+            HttpClient client = new HttpClient();
+            var message = new HttpRequestMessage(HttpMethod.Get, _uri);
+            var response = await client.SendAsync(message);
+            Console.WriteLine(response.StatusCode);
+
+            string content = await response.Content.ReadAsStringAsync();//JObject json = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+            Console.WriteLine(content);
+        }
+
+        [Test]
+        public async Task HttpClientHandlerTest()
         {
             HttpClient client = new HttpClient(new HttpClientHandler()
             {
                 ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
             });
-            var url = new Uri("https://www.baidu.com");
-            var message = new HttpRequestMessage(HttpMethod.Get, url);
+            var message = new HttpRequestMessage(HttpMethod.Get, _uri);
             var response = await client.SendAsync(message);
             Console.WriteLine(response.StatusCode);
-            Console.WriteLine(response.Content);
-            Console.WriteLine(response);
         }
     }
 }
