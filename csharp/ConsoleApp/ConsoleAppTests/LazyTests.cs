9ebc3086449dc35ede224e9c4379dc50181c8680
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2021-12-27 15:53:39 +0800

    Lazy

---
+++ csharp/ConsoleApp/ConsoleAppTests/LazyTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppTests
+{
+    public class LazyTests
+    {
+        [Test]
+        public void LazyBasicTest()
+        {
+            Lazy<Student> student = new Lazy<Student>();
+            if (!student.IsValueCreated)
+            {
+                Console.WriteLine("student is not Init");
+            }
+            string temp = string.IsNullOrEmpty(student.Value.Name) ? "null" : student.Value.Name;
+            Console.WriteLine($"Name: {temp}");
+            student.Value.Name = "dvbb";
+            student.Value.Id = 12033514;
+            Console.WriteLine(student.Value.Name);
+            Console.WriteLine(student.Value.Id);
+        }
+
+        private class Student
+        {
+            public string Name { get; set; }
+
+            public int Id { get; set; }
+
+            public Student() { }
+
+            public Student(string name, int id)
+            {
+                Name = name;
+                Id = id;
+            }
+        }
+    }
+}
