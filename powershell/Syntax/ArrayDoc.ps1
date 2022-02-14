e4e54a4f7cd3bb7b2519bea846c40ddb1a51f395
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-02-14 11:06:59 +0800

    Powershell Array

--- powershell/Syntax/ArrayDoc.ps1
+++ powershell/Syntax/ArrayDoc.ps1
@@ -1,5 +1,5 @@
 # ref: https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-arrays?view=powershell-7.2
-Write-Host -ForegroundColor Green "Start."
+Write-Host -ForegroundColor Green "`nStart."
 $data = 'Zero','One','Two','Three'
 $data = Write-Output Zero One Two Three four five six seven eight nine
 $data[0]
@@ -9,8 +9,20 @@ Write-Host $data[2..0]
 Write-Host $data[-1]
 Write-Host $data[2..-2]
 $null.count
-Start-Sleep 1
-Write-Host -ForegroundColor Green "Get the max index number:"
+
+# Start-Sleep 1
+Write-Host -ForegroundColor Green "`nGet the max index number:"
 Write-Host "GetUpperBound(0): " $data.GetUpperBound(0)
 Write-Host "index [GetUpperBound(0)]: " $data[$data.GetUpperBound(0)]
 Write-Host "index [-1]: " $data[-1]
+
+Write-Host -ForegroundColor Green "`nForEach & Select & Where"
+$data = @(
+    @{FirstName='john';LastName='Locke'}
+    @{FirstName='Sawyer'; LastName='Ford'}
+)
+Write-Host ($data | ForEach-Object {$_.FirstName , $_.LastName , "  "})
+Write-Host ($data | Where-Object {$_.LastName -eq "Locke"})
+Write-Host ($data | Where LastName -eq Locke)
+
+
---
+++ powershell/temp.ps1
@@ -0,0 +1,6 @@
+# $autorestVersion = "D:\repo\Changlong\autorest.csharp\artifacts\bin\AutoRest.CSharp\Debug\netcoreapp3.1"
+# $md = "D:\repo\azure-sdk-for-net\sdk\sqlvirtualmachine\Azure.ResourceManager.SqlVirtualMachine\mocktests" + "\autorest.tests.md"
+# & autorest --use=$autorestVersion $md --testmodeler="{}" --debug
+
+$processList = Get-Process
+$processList
