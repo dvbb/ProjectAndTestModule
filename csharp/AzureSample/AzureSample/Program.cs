9205a8158daf0922566dc848b7355eb72ce8a017
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-02-21 09:56:24 +0800

    new project

---
+++ csharp/AzureSample/AzureSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
