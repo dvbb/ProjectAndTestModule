349e1210668474505d1247a58deed5f965cad8ac
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-02-25 16:46:46 +0800

    new project

---
+++ csharp/AppFunction/AppFunction/ConvertFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFunction
+{
+    internal class ConvertFactory
+    {
+        internal void Convert(string str)
+        {
+
+        }
+    }
+}
