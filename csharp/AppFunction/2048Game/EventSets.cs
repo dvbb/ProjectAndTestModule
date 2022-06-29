d23db63b7e968f2bcf0864a4fa8226e0486f817e
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-06-29 16:42:00 +0800

    2048 game initial

---
+++ csharp/AppFunction/2048Game/EventSets.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace _2048Game
+{
+    public static class EventSets
+    {
+        public static int[,] InitArray(int length)
+        {
+            int[,] array = new int[length, length];
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    array[i, j] = 0;
+                }
+            }
+            return array;
+        }
+
+        public static void Show(int[,] array)
+        {
+            Console.Clear();
+            int length = (int)Math.Sqrt(array.Length);
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    Console.Write($"{array[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void NumberAppear(ref int[,] array)
+        {
+            int length = (int)Math.Sqrt(array.Length);
+            List<int> list = new List<int>();
+
+            // find empty grid
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if (array[i, j] == 0)
+                    {
+                        list.Add(i * length + j);
+                    }
+                }
+            }
+
+            Random rand = new Random();
+            int[] tempArray = list.ToArray();
+            if (list.Count == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Geme over!");
+                System.Environment.Exit(0);
+            }
+            int location = tempArray[rand.Next(list.Count)];
+            int row = location / length;
+            int column = location % length;
+            if (rand.Next(4)==1)    // 25% probability appear 4
+            {
+                array[row,column] = 2;
+            }
+            else
+            {
+                array[row, column] = 4;
+            }
+        }
+    }
+}
---
+++ csharp/AppFunction/2048Game/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static _2048Game.EventSets;
+
+namespace _2048Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 4;
+            int[,] array = InitArray(length);
+            while (true)
+            {
+                Show(array);
+                string key = (Console.ReadKey()).Key.ToString();
+                NumberAppear(ref array);
+                switch (key)
+                {
+                    case "S":
+                    case "DownArrow":
+                        Console.WriteLine("down Arrow event");
+                        break;
+                    case "W":
+                    case "UpArrow":
+                        Console.WriteLine("upArrow Arrow event");
+                        break;
+                    case "A":
+                    case "LeftArrow":
+                        Console.WriteLine("LeftArrow Arrow event");
+                        break;
+                    case "D":
+                    case "RightArrow":
+                        Console.WriteLine("RightArrow Arrow event");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
