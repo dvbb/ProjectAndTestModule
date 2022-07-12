a89875d758656a8183d18fa09880578df83f8398
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-07-12 15:47:30 +0800

    data lake

---
+++ csharp/AzureSample/DataLakeAnalytics/DataLakeTests.cs
@@ -0,0 +1,19 @@
+using AzureSample;
+using NUnit.Framework;
+
+namespace DataLakeAnalytics
+{
+    public class DataLakeTests : TestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void DataLakeTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
