55ba51d6485ab0674c7acb43b14b82f2d100520d
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-08-31 10:31:40 +0800

    IPAddress

---
+++ csharp/ConsoleApp/ConsoleAppTests/IPAddressTests.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleAppTests.Helper;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ConsoleAppTests
+{
+    internal class IPAddressTests : TestBase
+    {
+        [Test]
+        public void IPAddressTest()
+        {
+            string value1 = "192.168.0.1";
+            string value2 = "20.98.144.224/27";
+
+            IPAddress address1 = IPAddress.Parse(value1);
+            string log = RecordErrorMessage(() => IPAddress.Parse(value2));
+            Console.WriteLine(log);
+        }
+    }
+}
