b072d1765db94e40b853b023feca24af3c71c7a5
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-09-01 14:10:57 +0800

    update

--- csharp/ConsoleApp/ConsoleAppTests/IPAddressTests.cs
+++ csharp/ConsoleApp/ConsoleAppTests/IPAddressTests.cs
@@ -14,10 +14,13 @@ namespace ConsoleAppTests
         {
             string value1 = "192.168.0.1";
             string value2 = "20.98.144.224/27";
+            string value3 = "";
 
             IPAddress address1 = IPAddress.Parse(value1);
-            string log = RecordErrorMessage(() => IPAddress.Parse(value2));
-            Console.WriteLine(log);
+            string record1 = RecordErrorMessage(() => IPAddress.Parse(value2));
+            string record2 = RecordErrorMessage(() => IPAddress.Parse(value3));
+            Console.WriteLine($"{record1}\n\n");
+            Console.WriteLine($"{record2}\n\n");
         }
     }
 }
