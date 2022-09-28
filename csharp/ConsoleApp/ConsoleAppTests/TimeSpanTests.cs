2eb43df54290db3ac571b0f0b5b872f0e664f40b
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-09-28 13:42:58 +0800

    update

---
+++ csharp/ConsoleApp/ConsoleAppTests/TimeSpanTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppTests
+{
+    internal class TimespanTests
+    {
+        [Test]
+        public void TimespanTest()
+        {
+            TimeSpan time = new TimeSpan();
+            Console.WriteLine(time);
+
+            // ctor for one parameter is ticks
+            time = new TimeSpan(12);
+            Console.WriteLine(time);
+
+            time = new TimeSpan(315,25,28);
+            Console.WriteLine(time);
+            Console.WriteLine(time.Days);
+            Console.WriteLine(time.Hours);
+            Console.WriteLine(time.Seconds);
+            Console.WriteLine(time.Ticks);
+        }
+    }
+}
