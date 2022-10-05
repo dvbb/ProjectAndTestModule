74b66795dfbd9d57d28721604ae69422a59d3b2e
Chengming Ruan <v-cruan@microsoft.com>
2022-10-05 21:12:44 +0800

    optimize character movement act

--- Unity/FirstGame/Assets/Script/PlayerController.cs
+++ Unity/FirstGame/Assets/Script/PlayerController.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float Speed = 5f;
+
+    public Rigidbody2D rbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        System.Console.WriteLine(Speed);
+        rbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         // Horizontal movement to the right
         //transform.Translate(transform.right * Speed * Time.deltaTime);
@@ -21,9 +23,9 @@ public class MovementController : MonoBehaviour
         // Arror keys to control movement
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-        Vector2 position = transform.position;
-        position.x += moveX * Speed * Time.deltaTime;
-        position.y += moveY * Speed * Time.deltaTime;
-        transform.position = position;
+        Vector2 position = rbody.position;
+        position.x += moveX * Speed * Time.fixedDeltaTime;
+        position.y += moveY * Speed * Time.fixedDeltaTime;
+        rbody.MovePosition(position);
     }
 }
