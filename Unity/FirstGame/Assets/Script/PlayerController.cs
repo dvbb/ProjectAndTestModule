c1d6609cdf4116532ca4b5c6e7936ef233ff9312
Chengming Ruan <v-cruan@microsoft.com>
2022-10-19 21:38:36 +0800

    add ruby's amine and move control

--- Unity/FirstGame/Assets/Script/PlayerController.cs
+++ Unity/FirstGame/Assets/Script/PlayerController.cs
@@ -16,6 +16,10 @@ public class PlayerController : MonoBehaviour
     private float invincibleTime = 2f;
     private float invincibleTimer;
     private bool isInvincible;
+    private Animator animator;
+
+    // Player Directions
+    private Vector2 playerDirection = new Vector2(0, 1);
 
     // Start is called before the first frame update
     void Start()
@@ -24,17 +28,30 @@ public class PlayerController : MonoBehaviour
         invincibleTimer = 0;
         isInvincible = false;
         rbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     public void Update()
     {
-        // Arror keys to control movement
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        // Arrow keys to control movement
+        float moveX = Input.GetAxisRaw("Horizontal");   // [A] -1 [D]  1
+        float moveY = Input.GetAxisRaw("Vertical");     // [W] 1  [S] -1
+
+        // Set player direction with the arrow keys
+        Vector2 moveVector = new Vector2(moveX, moveY);
+        if (moveX != 0 || moveY != 0)
+        {
+            playerDirection = moveVector;
+            animator.SetFloat("Look X", playerDirection.x);
+            animator.SetFloat("Look Y", playerDirection.y);
+        }
+        animator.SetFloat("Speed", moveVector.magnitude);
+
         Vector2 position = rbody.position;
-        position.x += moveX * Speed * Time.fixedDeltaTime;
-        position.y += moveY * Speed * Time.fixedDeltaTime;
+        //position.x += moveX * Speed * Time.fixedDeltaTime;
+        //position.y += moveY * Speed * Time.fixedDeltaTime;
+        position += moveVector * Speed * Time.fixedDeltaTime;
         rbody.MovePosition(position);
 
         // update invincible time
