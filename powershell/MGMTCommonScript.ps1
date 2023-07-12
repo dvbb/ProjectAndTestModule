6e0ac46d72f464825cf99234c6cf0b78690c8811
Chengming <v-cruan@microsoft.com>
2023-07-12 15:39:26 +0800

    Create MGMTCommonScript.ps1

---
+++ powershell/MGMTCommonScript.ps1
@@ -0,0 +1,60 @@
+function  Find-SolutionPaths {
+    param(
+        [Parameter()]
+        [string]$sdkRootPath = ""
+    )
+    process {
+        $filterList = Get-ChildItem -Path $sdkRootPath -Recurse -Filter CHANGELOG.md | % { $_.FullName }
+        $paths = $filterList | % {
+            if ($_.Contains("Azure.ResourceManager")) { $_.Replace("CHANGELOG.md", "") }
+        }
+        return $paths
+    }
+}
+
+function  Invoke-MGMTScript {
+    param(
+        [Parameter()]
+        [array]$cmds, 
+        [Parameter()]
+        [string]$fileStorePath = "C:\Users\v-cruan\Documents\GitHub\doc\Script"
+    )
+    process {
+        $paths = Find-SolutionPaths
+        $SuccessRPs = @()
+        $FailedRPs = @()
+        foreach ($path in $paths) {
+            Set-Location $path
+            foreach ($cmd in $cmds) {
+                Invoke-Expression $cmd
+            }
+            if ($?) {
+                $SuccessRPs += $path
+                $SuccessRPs | Out-File -FilePath "$fileStorePath\SuccessRPs.txt"
+            }
+            else {
+                $FailedRPs += $path
+                $FailedRPs | Out-File -FilePath "$fileStorePath\FailedRPs.txt"
+            }
+        }
+    }
+}
+
+$test = @(
+    "dotnet test -f net7.0"
+)
+$buildAllSolution = @(
+    "dotnet build"
+)
+$buildAllSamples = @(
+    "cd samples",
+    "dotnet build"
+)
+$buildAllSrc = @(
+    "cd src",
+    "dotnet build"
+)
+$restoreAllRecordingFiles = @(
+    "test-proxy restore -a ./assets.json"
+)
+Invoke-MGMTScript -cmd $restoreAllRecordingFiles
