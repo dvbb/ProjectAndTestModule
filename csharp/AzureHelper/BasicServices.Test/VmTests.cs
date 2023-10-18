5f67a55a46f31df626555fe355c4de5b60f7ef29
Chengming <v-cruan@microsoft.com>
2023-10-18 13:31:51 +0800

    Update VmTests.cs

--- csharp/AzureHelper/BasicServices.Test/VmTests.cs
+++ csharp/AzureHelper/BasicServices.Test/VmTests.cs
@@ -27,11 +27,19 @@ namespace BasicServices.Tests
         [SetUp]
         public async Task SetUp()
         {
-            // Create a resource group
-            _rgName = BaseClientExtension.CreateRandomName("vm");
+            _rgName = BaseClientExtension.CreateRandomName("VMRG");
             BaseClient baseClient = new BaseClient();
             resourceGroup = await baseClient.CreateResourceGroup(_rgName);
+        }
 
+        [TearDown]
+        public async Task TearDown()
+        {
+            resourceGroup.Delete(WaitUntil.Started);
+        }
+
+        private async Task CreateVmPrepare()
+        {
             // Create a NIC
             var vnet = await NetworkHelper.CreateVirtualNetwork(resourceGroup);
             var publicIP = await NetworkHelper.CreatePublicIP(resourceGroup);
@@ -42,15 +50,11 @@ namespace BasicServices.Tests
             _vmName = BaseClientExtension.CreateRandomName("vm");
         }
 
-        [TearDown]
-        public async Task TearDown()
-        {
-            resourceGroup.Delete(WaitUntil.Started);
-        }
-
         [Test]
         public async Task Linux_Pwd()
         {
+            await CreateVmPrepare();
+
             VirtualMachineData vmInput = VirtualMachineHelper.GetInputData_Linux_Pwd(resourceGroup, _vmName);
             vmInput.NetworkProfile.NetworkInterfaces.Add(
                 new VirtualMachineNetworkInterfaceReference()
@@ -67,6 +71,8 @@ namespace BasicServices.Tests
         [Test]
         public async Task Windows()
         {
+            await CreateVmPrepare();
+
             VirtualMachineData vmInput = VirtualMachineHelper.GetInputData_Windows(resourceGroup, _vmName);
             vmInput.NetworkProfile.NetworkInterfaces.Add(
                 new VirtualMachineNetworkInterfaceReference()
@@ -78,6 +84,28 @@ namespace BasicServices.Tests
             VirtualMachineResource vm = vmLro.Value;
             await Console.Out.WriteLineAsync(vm.Data.Name);
             await Console.Out.WriteLineAsync(vm.Data.StorageProfile.OSDisk.OSType.ToString());
+        }
+
+        [Test]
+        public async Task Disk()
+        {
+            string diskName = BaseClientExtension.CreateRandomName("disk");
+            ManagedDiskData diskInput1 = new ManagedDiskData(resourceGroup.Data.Location)
+            {
+                Sku = new DiskSku()
+                {
+                    Name = DiskStorageAccountType.PremiumLrs
+                },
+                Zones = { "1" }, // Zone must be bigger than 0; otherwise, it cannot be attached to the VM
+                CreationData = new DiskCreationData(DiskCreateOption.Empty),
+                DiskSizeGB = 1024,
+                DiskMBpsReadWrite = 200,
+                Encryption = new DiskEncryption() { EncryptionType = ComputeEncryptionType.EncryptionAtRestWithPlatformKey },
+                PublicNetworkAccess = DiskPublicNetworkAccess.Enabled,
+            };
+            var diskLro = await resourceGroup.GetManagedDisks().CreateOrUpdateAsync(WaitUntil.Completed, diskName, diskInput1);
+            ManagedDiskResource disk = diskLro.Value;
+            await Console.Out.WriteLineAsync(disk.Data.Name);
         }
     }
 }
