fc3bf427d25731cc955a74e81ac2c00e5c1c93a6
Chengming Ruan <578760600@qq.com>
2024-05-26 15:41:08 +0800

    research construction method

---
+++ csharp/ConsoleApp/ConsoleApp/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Models
+{
+    internal class Person
+    {
+        public static string _StaticField  = "value1";
+        public static string StaticField1 { get; set; } = "value1";
+
+        public Person()
+        {
+            Console.WriteLine("Person construction method");
+        }
+        public static string StaticField2 { get; set; } = "value2";
+
+    }
+}
---
+++ csharp/ConsoleApp/ConsoleApp/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Models
+{
+    internal class Student : Person
+    {
+        public static string StaticField3 { get; set; } = "value3";
+        public Student()
+        {
+            Console.WriteLine("Student construction method");
+        }
+        public static string StaticField4 { get; set; } = "value4";
+
+    }
+}
