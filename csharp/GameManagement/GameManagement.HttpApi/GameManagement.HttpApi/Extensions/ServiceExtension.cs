26418ed152bdee6767c47ccbac298fdd8216c97c
Chengming Ruan <578760600@qq.com>
2024-05-28 21:37:31 +0800

    Init new project

---
+++ csharp/GameManagement/GameManagement.HttpApi/GameManagement.HttpApi/Extensions/ServiceExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Identity.Client;
+
+namespace GameManagement.Extensions
+{
+    public static class ServiceExtension
+    {
+        /// <summary>
+        /// config cross-domain
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(option =>
+            {
+                option.AddPolicy("AnyPolicy",
+                    builder => builder.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin());
+            });
+        }
+    }
+}
