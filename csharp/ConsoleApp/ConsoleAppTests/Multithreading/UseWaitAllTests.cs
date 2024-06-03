b3b3107dba49ead13d6fdb51c71b2e10423c34ec
Chengming Ruan <578760600@qq.com>
2024-06-03 10:46:34 +0800

    Update UseWaitAllTests.cs

--- csharp/ConsoleApp/ConsoleAppTests/Multithreading/UseWaitAllTests.cs
+++ csharp/ConsoleApp/ConsoleAppTests/Multithreading/UseWaitAllTests.cs
@@ -3,13 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp.Multithreading.Tests
 {
     public class UseWaitAllTests
     {
-        private UseWaitAll _member;
+        private static UseWaitAll _member;
 
         [SetUp]
         public void SetUp()
@@ -18,18 +19,26 @@ namespace ConsoleApp.Multithreading.Tests
         }
 
         [Test]
-        public void ShowTest()
+        public void WaitAllTest()
         {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-
             // 单个 Task 执行 需要5-8s.
             // 将多个 Task 放入 Task[] 中，然后执行Task.WaitAll.
             // 此时 Task[] 中的所有任务会并发运行，同时所有的任务完成前，主线程会阻塞在此行.
-            Task[] tasks = new Task[] { _member.Show("this is t1"), _member.Show("this is t2") };
-            Task.WaitAll(tasks);
+            Action action = () =>
+            {
+               Task[] tasks = new Task[] { _member.Show("this is t1"), _member.Show("this is t2") };
+               Task.WaitAll(tasks);
+            };
+            Watch(action);
+        }
+
+        private static void Watch(Action action)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            action.Invoke();
+            sw.Stop();
             Console.WriteLine(sw.Elapsed);
-            Assert.IsTrue(sw.Elapsed.TotalSeconds<8);
         }
     }
 }
